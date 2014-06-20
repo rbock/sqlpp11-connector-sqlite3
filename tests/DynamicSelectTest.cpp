31284632d1018fc6b6ad1a44e3d34032836a2f5d
rbock <rbock@eudoxos.de>
2014-06-20 18:56:26 +0200

    Adjusted to recent changes in sqlpp11

--- tests/DynamicSelectTest.cpp
+++ tests/DynamicSelectTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Roland Bock
+ * Copyright (c) 2013-2014, Roland Bock
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
@@ -59,14 +59,14 @@ int main()
 	TabSample tab;
 
 	auto i = insert_into(tab).columns(tab.beta, tab.gamma);
-	i.add_values(tab.beta = "rhabarbertorte", tab.gamma = false);
-	//i.add_values(tab.beta = "cheesecake", tab.gamma = false)
-	//i.add_values(tab.beta = "kaesekuchen", tab.gamma = true)
+	i.values.add(tab.beta = "rhabarbertorte", tab.gamma = false);
+	//i.values.add(tab.beta = "cheesecake", tab.gamma = false)
+	//i.values.add(tab.beta = "kaesekuchen", tab.gamma = true)
 	db.run(i);
 
 	// select a static (alpha) and a dynamic column (beta)
 	auto s = dynamic_select(db).dynamic_columns(tab.alpha).from(tab).where(true);
-	s.add_column(tab.beta);
+	s.selected_columns.add(tab.beta);
 	for(const auto& row : db.run(s))
 	{
 		std::cerr << "row.alpha: " << row.alpha << ", row.beta: " << row.at("beta") << std::endl;
