11ef903c3d265281e7fd80282619285343f3fa0a
JUAN DENT <juan_dent@hotmail.com>
2017-12-19 15:51:06 -0600

    Added copyright

--- tests/SelectTest.cpp
+++ tests/SelectTest.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013 - 2016, Roland Bock
+ * Copyright (c) 2017, Juan Dent
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
