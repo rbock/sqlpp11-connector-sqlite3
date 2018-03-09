53d211d2d498449d7c47b78c770eb877e551dcae
Théo DELRIEU <theo.delrieu@tanker.io>
2018-03-10 00:27:04 +0100

    use find_package and targets for sqlpp11
    also export targets/config files for sqlpp11-connector-sqlite3{-dynamic}

---
+++ cmake/Sqlpp11ConnectorSqlite3Config.cmake
@@ -0,0 +1,6 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+
+include(CMakeFindDependencyMacro)
+find_dependency(Sqlpp11 REQUIRED)
+
+include("${CMAKE_CURRENT_LIST_DIR}/Sqlpp11ConnectorSqlite3Targets.cmake")
